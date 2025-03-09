175ec41f2fe7fbc09be4e25c14e8b53037a4d00c
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-09 21:13:50 +0300

    a

--- Adding Game/ViewModels/HighScoreViewModel.swift
+++ Adding Game/ViewModels/HighScoreViewModel.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import CoreData
+
+
+class HighScoreViewModel : ObservableObject {
+    
+    let container: NSPersistentContainer
+    
+    @Published var highScores: [HighScoreEntity] = []
+    
+    init() {
+        container = NSPersistentContainer(name: "HighScoreDataModel")
+        
+        container
+            .loadPersistentStores{ description, error in
+            
+                if let error {
+                    print("Loading error: \(error.localizedDescription)")
+                }else{
+                    print("Loading succesful")
+                }
+                
+            }
+        
+        //TODO: Perpahs fetch data is ascending(descending order
+        func fetchHighScores(){
+            
+            let request = NSFetchRequest<HighScoreEntity>(entityName: "HighScoreEntity")
+            
+            do {
+                highScores = try container.viewContext.fetch(request)
+
+            }catch let error {
+                print("Error fetching high socres: \(error.localizedDescription)")
+            }
+            
+            
+        }
+        
+    }
+    
+}
