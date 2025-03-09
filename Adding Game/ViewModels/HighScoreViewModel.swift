48437a2fb121f4d541a549c474f22cd531cd1035
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-09 21:35:00 +0300

    ab

--- Adding Game/ViewModels/HighScoreViewModel.swift
+++ Adding Game/ViewModels/HighScoreViewModel.swift
@@ -28,22 +28,56 @@ class HighScoreViewModel : ObservableObject {
                 }
                 
             }
+      
+    }
+    
+    func fetchHighScores(){
         
-        //TODO: Perpahs fetch data is ascending(descending order
-        func fetchHighScores(){
-            
-            let request = NSFetchRequest<HighScoreEntity>(entityName: "HighScoreEntity")
-            
-            do {
-                highScores = try container.viewContext.fetch(request)
+        let request = NSFetchRequest<HighScoreEntity>(entityName: "HighScoreEntity")
+        let sortDescriptor = NSSortDescriptor(keyPath: \HighScoreEntity.score, ascending: false)
+        
+        request
+            .sortDescriptors = [sortDescriptor]
+        
+        do {
+            highScores = try container.viewContext.fetch(request)
 
-            }catch let error {
-                print("Error fetching high socres: \(error.localizedDescription)")
-            }
-            
-            
+        }catch let error {
+            print("Error fetching high scores: \(error.localizedDescription)")
         }
         
     }
+    func saveHighScore() {
+        do {
+           try container
+                .viewContext
+                .save()
+            fetchHighScores()
+        }catch let error {
+            print("Error saving high scores: \(error.localizedDescription)")
+        }
+    }
     
+    func addHighScore(name: String, score: Int64){
+        let entity = HighScoreEntity(context: container.viewContext)
+        entity.name = name
+        entity.score = score
+        saveHighScore()
+        
+    }
+    func updateHighScore(entity: HighScoreEntity ,name: String){
+        entity.name = name
+       
+        saveHighScore()
+    }
+    
+    func deleteHighScore(entity: HighScoreEntity){
+        
+        container.viewContext.delete(entity)
+       
+        saveHighScore()
+    }
+    
+
+        
 }
