f6d7447530f3a6cc38fe0f0fa89e90d56c647fa8
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-10 15:05:32 +0300

    c

--- Adding Game/Model/ProblemModel.swift
+++ Adding Game/Model/ProblemModel.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+struct ProblemModel : Identifiable {
+    let id = UUID()
+    let number1: Int
+    let number2: Int
+    let level: Int
+    
+    
+    let solutions: [Int]
+    
+    var answer: Int {
+        solutions[0]
+    }
+    
+    
+    func checkSolution(value: Int) -> Bool {
+        !solutions.isEmpty && value == answer
+    }
+    
+    
+    init(level: Int) {
+        self.level = level
+        let lower = (level - 1) * 5
+        let upper = level * 5
+        self.number1 = Int.random(in: lower...upper)
+        self.number2 = Int.random(in: lower...upper)
+        let answer = number1 + number2
+        
+        var newSolution: [Int] = []
+        
+        newSolution.append(answer) // true solution at solution[0] = 27
+        newSolution.append(answer + 10) // Distraction = 37
+        
+        let belowAnswer = max(0,answer - 5)
+        let aboveAnswer = answer + 5
+        
+        newSolution.append(Int.random(in: answer+1...aboveAnswer))
+        newSolution.append(Int.random(in: belowAnswer..<answer))
+        
+        self.solutions = newSolution
+        
+        
+    }
+    
+}
