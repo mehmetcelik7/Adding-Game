e24472e9b868714bd77344585423e657cbb88cd3
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-11 12:01:40 +0300

    f

---
+++ Adding Game/View/High Score View/HighScoreTitle.swift
@@ -0,0 +1,18 @@
+//
+//  HighScoreTitle.swift
+//  Adding Game
+//
+//  Created by mehmet Çelik on 11.03.2025.
+//
+
+import SwiftUI
+
+struct HighScoreTitle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HighScoreTitle()
+}
